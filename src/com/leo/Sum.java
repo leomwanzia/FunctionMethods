aced7f0d65ed361de1d8cc7aac1d8ed8f9bc460a
leo <leomutungi19@gmail.com>
2024-10-10 12:28:30 +0300

    second commit

--- src/com/leo/Sum.java
+++ src/com/leo/Sum.java
@@ -5,8 +5,24 @@ import java.util.Scanner;
 public class Sum {
 
     public static void main(String[] args) {
-        sum();
+        int ans = sum2();
+        System.out.println(ans);
     }
+
+    //return the value
+    static int sum2() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = in.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = in.nextInt();
+
+        int sum = num1 + num2;
+        return sum;
+    }
+
     static void sum() {
         Scanner in = new Scanner(System.in);
 
